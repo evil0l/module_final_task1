5cb27c454005f75414fe64fee4a24eabd26f9840
evil0l <139062839+evil0l@users.noreply.github.com>
2023-07-10 21:38:06 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,14 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-
-# 1. окрыть главную страницу
-# 2. открыть страницу логин
-# 3. заполнить поле email
-# 4. заполнить поле password
-# 5. нажать кнопку start
 
 URL = 'https://www.saucedemo.com/'
 LOGIN = 'standard_user'
@@ -16,22 +11,35 @@ PASSWORD = 'secret_sauce'
 
 
 def get_driver():
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    chrome_options = Options()
+    chrome_options.add_argument("--window-size=1920,800")
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),
+                              options=chrome_options)
+    driver.maximize_window()
+    driver.implicitly_wait(10)
     return driver
 
 
 def open_page(driver, url):
     driver.get(url)
 
+def open_login_page():
+    element_click(xpath='login-link', driver=driver)
+
+def element_click(xpath, driver):
+    element = get_element_by_id(xpath=xpath, driver=driver)
+    element.click()
+
+def login(login, password):
+    element_send_keys(xpath="user-name", driver=driver, text=login)
+    element_send_keys(xpath="password", driver=driver, text=password)
+    element_click(xpath='login-button', driver=driver)
 
 
 def get_element_by_id(xpath, driver):
     element = driver.find_element(By.ID, xpath)
     return element
-
-def element_click(xpath, driver):
-    element = get_element_by_id(xpath=xpath, driver=driver)
-    element.click()
 
 def element_send_keys(xpath, driver, text):
     element = get_element_by_id(xpath=xpath, driver=driver)
@@ -41,7 +49,5 @@ def element_send_keys(xpath, driver, text):
 # 1
 driver = get_driver()
 open_page(driver, URL)
-#element_click(xpath='login-link', driver=driver)
-element_send_keys(xpath="user-name", driver=driver, text=LOGIN)
-element_send_keys(xpath="password", driver=driver, text=PASSWORD)
-element_click(xpath='login-button', driver=driver)
+open_login_page()
+login(login=LOGIN, password=PASSWORD)
